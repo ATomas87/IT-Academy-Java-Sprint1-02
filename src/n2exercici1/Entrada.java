f7f4d80deeb2903685c05fadb80387362c3137ba
ATomas87 <kombayn_nikoladze@msn.com>
2022-04-29 12:59:10 +0200

    Nivel 2 completado

--- src/n2exercici1/Entrada.java
+++ src/n2exercici1/Entrada.java
@@ -18,34 +18,108 @@ public class Entrada {
                 userByte = scanner.nextByte();
                 correct = true;
             } catch (InputMismatchException e) {
-                System.out.println("El dato introducido no se corresponde con el tipo de dato esperado.");
+                System.out.println("Error. Asegúrate de introducir sólo un byte.");
                 scanner.nextLine();
             }
         } while (!correct);
         return userByte;
     }
 
-//    public static int readInt(String message) {
-//
-//    }
-//
-//    public static float readFloat(String message) {
-//
-//    }
-//
-//    public static double readDouble(String message) {
-//
-//    }
-//
-//    public static char readChar(String message) {
-//
-//    }
-//
-//    public static String readString(String message) {
-//
-//    }
-//
-//    public static boolean readYesNo(String message) {
-//
-//    }
+    public static int readInt(String message) {
+        int userInt = 0;
+        boolean correct = false;
+        do {
+            System.out.println(message);
+            try {
+                userInt = scanner.nextInt();
+                correct = true;
+            } catch (InputMismatchException e){
+                System.out.println("Error. Has introducido un dato que no se corresponde con un número entero.");
+                scanner.nextLine();
+            }
+        } while (!correct);
+        return userInt;
+    }
+
+    public static float readFloat(String message) {
+        float userFloat = 0;
+        boolean correct = false;
+        do {
+            System.out.println(message);
+            try {
+                userFloat = scanner.nextFloat();
+                correct = true;
+            } catch (InputMismatchException e){
+                System.out.println("Error. Asegúrate de utilizar una coma en lugar de un punto para introducir el número decimal.");
+                scanner.nextLine();
+            }
+        } while (!correct);
+        return userFloat;
+    }
+
+    public static double readDouble(String message) {
+        double userDouble = 0;
+        boolean correct = false;
+        do {
+            System.out.println(message);
+            try {
+                userDouble = scanner.nextDouble();
+                correct = true;
+            } catch (InputMismatchException e){
+                System.out.println("Error. Asegúrate de utilizar una coma en lugar de un punto para introducir el número decimal.");
+                scanner.nextLine();
+            }
+        } while (!correct);
+        return userDouble;
+    }
+
+    public static char readChar(String message) {
+        char userChar = Character.MIN_VALUE;
+        boolean correct = false;
+        do {
+            System.out.println(message);
+            try {
+                userChar = scanner.next(".").charAt(0);
+                correct = true;
+            } catch (Exception e){
+                System.out.println("Error. Has introducido más de un carácter.");
+                scanner.nextLine();
+            }
+        } while (!correct);
+        return userChar;
+    }
+
+    public static String readString(String message) {
+        String userString = "";
+        boolean correct = false;
+        do {
+            System.out.println(message);
+            try {
+                userString = scanner.nextLine();
+                correct = true;
+            } catch (Exception e){
+                System.out.println("Error. El dato introducido no es una cadena de texto.");
+                scanner.nextLine();
+            }
+        } while (!correct);
+        return userString;
+    }
+
+    public static boolean readYesNo(String message) {
+        char userBoolean;
+        do {
+            System.out.println(message);
+            try {
+                userBoolean = scanner.next("[sSnN]").charAt(0);
+                if (userBoolean == 's' || userBoolean == 'S'){
+                    return true;
+                } else if (userBoolean == 'n' || userBoolean == 'N'){
+                    return false;
+                }
+            } catch (Exception e){
+                System.out.println("Error. Asegúrate de responder correctamente (S/N).");
+                scanner.nextLine();
+            }
+        } while (true);
+    }
 }
--- src/n2exercici1/Main.java
+++ src/n2exercici1/Main.java
@@ -3,5 +3,12 @@ package n2exercici1;
 public class Main {
     public static void main(String[] args) {
         Entrada.readByte("Introduce un byte:");
+        Entrada.readChar("Introduce un carácter:");
+        Entrada.readDouble("Introduce un número con decimales:");
+        Entrada.readFloat("Introduce un número con decimales:");
+        Entrada.readInt("Introduce un número entero:");
+        Entrada.readString("Introduce una frase:");
+        Entrada.readYesNo("¿Te gustan los macarrones? S/N:");
     }
+
 }
